5f06b4aa03d02a4801a496a7ae49efa789873168
Ahmshere <asus555post@gmail.com>
2024-03-18 15:57:09 +0200

    Resolved2

--- src/test/java/tests/TestClass.java
+++ src/test/java/tests/TestClass.java
@@ -2,10 +2,7 @@ package tests;
 
 import config.BaseTest;
 import org.testng.annotations.Test;
-import pages.HomePage;
-import pages.LetTheCarWorkPage;
-import pages.LogInPage;
-import pages.TopMenuItem;
+import pages.*;
 
 public class TestClass extends BaseTest {
 
@@ -17,8 +14,6 @@ public class TestClass extends BaseTest {
        LetTheCarWorkPage lcwp = logInPage.openTopMenu(TopMenuItem.LET_THE_CAR_WORK);
        lcwp.setManufacturer("BMW").addLocation("Montana")
                .setFuelType().photoAttachment("C:\\Pictures\\BlueCar.jpg");
-
-
        Thread.sleep(3000);
     }
     @Test
